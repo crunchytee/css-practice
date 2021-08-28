9be6d2fa9621234ca4590245a370fb9cf33d62bd
crunchytee <tosh.robertsbrockway@gmail.com>
2021-08-28 16:02:46 -0700

    added elements and base styled row 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>HELLO WORLD</div>
+    <div id="wrapper" className="m-6 h-screen w-screen">
+      {/* <div id="rectangle-1" className="w-3/12 h-1/2 fill-current text-green-600"></div> */}
+      <div id="row-1-wrapper" className="flex justify-end w-screen">
+        <div id="rectangle-1" className="row-1-rectangle"/>
+        <div id="rectangle-2" className="row-1-rectangle"/>
+        <div id="rectangle-3" className="row-1-rectangle"/>
+      </div>
+      <div id="row-2-wrapper">
+        <div id="rectangle-4" />
+        <div id="circle-1" />
+      </div>
+      <div id="row-3-wrapper">
+        <div id="square-1" />
+        <div id="line-wrapper">
+          <div id="line-1" />
+          <div id="line-2" />
+          <div id="line-3" />
+        </div>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,30 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    height: {
+      "100px": "100px",
+      "200px": "200px",
+      "300px": "300px",
+      "400px": "400px",
+    },
+    width: {
+      "100px": "100px",
+      "200px": "200px",
+      "300px": "300px",
+      "400px": "400px",
+    },
+    extend: {
+      spacing: {
+        "100px": "100px",
+        "200px": "200px",
+        "300px": "300px",
+        "400px": "400px",
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }

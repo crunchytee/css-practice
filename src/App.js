1e196716948ebde314ecfb626e0d0bb8cf50203f
crunchytee <tosh.robertsbrockway@gmail.com>
2021-08-28 19:51:59 -0700

    Added row two styling

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import './App.css';
 
 function App() {
   return (
-    <div id="wrapper" className="m-6 h-screen w-screen">
-      {/* <div id="rectangle-1" className="w-3/12 h-1/2 fill-current text-green-600"></div> */}
-      <div id="row-1-wrapper" className="flex justify-end w-screen">
+    <div id="row-wrapper" className="m-6 h-screen w-screen">
+      <div id="row-1-wrapper" className="flex justify-around w-screen m-6">
         <div id="rectangle-1" className="row-1-rectangle"/>
         <div id="rectangle-2" className="row-1-rectangle"/>
         <div id="rectangle-3" className="row-1-rectangle"/>
       </div>
-      <div id="row-2-wrapper">
-        <div id="rectangle-4" />
-        <div id="circle-1" />
+      <div id="row-2-wrapper" className="flex justify-around w-screen mt-28">
+        <div id="rectangle-4" className="w-700px h-100px bg-green-600 mr-64"/>
+        <div id="circle-1" className="w-100px h-100px bg-green-600 rounded-full"/>
       </div>
-      <div id="row-3-wrapper">
+      <div id="row-3-wrapper" className="flex justify-around w-screen">
         <div id="square-1" />
         <div id="line-wrapper">
           <div id="line-1" />
